d9877f5fc60881c99a9f7b0970fba5fbd273ea4e
Nikolai Koenig <95688889+akiralog@users.noreply.github.com>
2025-08-24 22:20:58 +0100

    Remove junk

--- conditions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ThamesTown
-
-
-@admin.register(ThamesTown)
-class ThamesTownAdmin(admin.ModelAdmin):
-    list_display = ['town_name', 'location_id', 'is_tidal', 'created_at']
-    list_filter = ['is_tidal']
-    search_fields = ['town_name']
--- conditions/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import ThamesTown
-
-
-def index(request):
-    """Main index view - serves the frontend"""
-    towns = ThamesTown.objects.all()
-    return render(request, 'index.html', {'towns': towns})
