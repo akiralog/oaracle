531a08d6306467cd9fdd61051b75743b46da5c7a
Nikolai Koenig <95688889+akiralog@users.noreply.github.com>
2025-08-24 23:24:29 +0100

    Fetch data through WeatherManager

--- oaracle_app/views.py
+++ oaracle_app/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import sqlite3
 from pathlib import Path
+from .db import DatabaseManager
+from .weather import WeatherManager
+import os
 
-
+API_KEY = os.getenv('WILLY_WEATHER_API_KEY')
 BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PATH = BASE_DIR / 'locations.db'
+
+wm = WeatherManager(API_KEY)
 
 def index(request):
     return render(request, "index.html")
@@ -20,3 +25,21 @@ def get_towns(req):
     conn.close()
     return JsonResponse({"towns": towns})
     
+def fetch_data(request):
+    location_id = request.GET.get("location_id")
+    is_tidal = request.GET.get("is_tidal") == "true"
+    
+    if not location_id:
+        return JsonResponse({"error": "Missing location_id"}, status=400)
+    
+    location_id = int(location_id)
+    
+    weather_data = wm.get_weather_data(location_id, days=1)
+    tide_data = None
+    if is_tidal:
+        tide_data = wm.get_tide_data(location_id, days=1)
+        
+    return JsonResponse({
+        "weather": weather_data,
+        "tides": tide_data
+    })
